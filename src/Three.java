cbcd2a20b0efec7a55817913aa001a7346310b6d
Ravi Teja <raviteja.valaboju@gmail.com>
2020-12-14 19:14:04 +0530

    American server 1

---
+++ src/Three.java
@@ -0,0 +1,13 @@
+
+public class Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ONE");
+		System.out.println("TWO");
+		System.out.println("THREE");
+
+	}
+
+}
---
+++ src/Two.java
@@ -0,0 +1,13 @@
+
+public class Two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ONE");
+		System.out.println("TWO");
+		System.out.println("THREE");
+
+	}
+
+}
